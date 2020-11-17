b4a3c999256f634dd3de3f41ab8be3a32e42130d
brandonworks <work@brandontorres.dev>
2020-11-17 12:37:45 -0800

    commit tree e89c8de2135c4cd217d2e5b0c0e9d78fd8748a6f

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+#objectives:
+#take in an argument of a collection
+#iterate over a collection using a while loop
+#execute the code we call for each item in collection using yield
+def my_collect(collection) 
+  i = 0
+  collect = []
+  while i < collection.length
+    collect << yield(collection[i])
+    i += 1
+  end
+  collect
+end
